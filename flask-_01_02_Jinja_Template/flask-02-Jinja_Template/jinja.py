c13ffde0cfccb225ea1bf22239bbe67806d6e56a
cnrgrl <cnrgrl@gmail.com>
2024-03-14 10:55:32 +0100

    route added

--- flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -2,7 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def head():
+     return render_template('index.html', number1=35,number2=36)
 
+@app.route('/sum')
+def sum():
+     x=35
+     y=40
+     return render_template('body.html', value1=x, value2=y, sum=x+y)
 
 
 if __name__== "__main__":
