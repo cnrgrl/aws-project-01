4fc7ac237b14333dd8d71f15acba4e1afba4c084
cnrgrl <cnrgrl@gmail.com>
2024-04-18 12:20:09 +0200

    secret manager config

--- flask-05-Handling-SQL-with-Flask-Web-Application/Solutions/Optional_Solution_with_Secrets_manager/app-with-secret-manager.py
+++ flask-05-Handling-SQL-with-Flask-Web-Application/Solutions/Optional_Solution_with_Secrets_manager/app-with-secret-manager.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # Function to retrieve secrets from AWS Secrets Manager
 def get_secret():
-    secret_name = "oktayflask"
+    secret_name = "cnrsecretflask05"
     region_name = "us-east-1"
 
     session = boto3.session.Session()
