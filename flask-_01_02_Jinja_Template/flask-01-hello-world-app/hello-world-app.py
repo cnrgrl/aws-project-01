469ef98779b9b1095d7af03c99adc70594b8b1d3
caner <66187631+cnrgrl@users.noreply.github.com>
2024-06-02 13:47:01 +0200

    improve

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,6 +1,12 @@
-from flask import Flask 
+from flask import Flask, abort, request
 
-app = Flask(__name__)
+APP_NAME = 'My Application'
+APP_VERSION = '0.1.0'
+ABOUT_MESSAGE = 'This is the about page of my application.'
+SECRET_KEY = 'my-secret-key'
+
+app = Flask(APP_NAME)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
 def head():
@@ -11,16 +17,44 @@ def head():
 def second():
     return 'This is second page'
 
+
 @app.route('/third')
 def third():
     return 'This is third page'
+
 
 @app.route('/forth/<string:id>')
 def forth(id):
     return f'Id of this page is {id}'
 
 
-if __name__ == '__main__':
+@app.route('/about')
+def about():
+    return ABOUT_MESSAGE
 
-    # app.run(debug=True)
-      app.run(host= '0.0.0.0/0', port=80)
+
+@app.route('/not-found')
+def not_found():
+    return 'This page was not found', 404
+
+
+@app.route('/version')
+def version():
+    return APP_VERSION
+
+
+@app.route('/health')
+def health():
+    return 'OK'
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return not_found()
+
+
+if __name__ == '__main__':
+    docstring = f'{APP_NAME} ({APP_VERSION})'
+    print(docstring)
+    print('Starting server on http://0.0.0.0/0:80')
+    app.run(debug=True, host='0.0.0.0/0', port=80)
