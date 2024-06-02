932e7b5180b207499eb39f2393ec58f2f6135a2e
caner <66187631+cnrgrl@users.noreply.github.com>
2024-06-02 13:46:54 +0200

    improve

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
@@ -3,25 +3,22 @@ from flask import Flask, render_template
 # Create an object named app 
 app = Flask(__name__)
 
-# Create a function named head which shows the massage as "This is my first conditions experience" in `index.html` 
+# Create a function named head which shows the message as "This is my first conditions experience" in `index.html` 
 # and assign to the route of ('/')
 @app.route("/")
 def head():
-    first="This is my first conditions experience"
-    return render_template("index.html", message=first)
+    message = "This is my first conditions experience"
+    return render_template("index.html", message=message)
 
 
 # Create a function named header which prints numbers elements of list one by one in `index.html` 
-# and assign to the route of ('/')
+# and assign to the route of ('/list')
 @app.route("/list")
 def header():
-    # names =["tom", "osvaldo", "guile", "tommy"]
-    numbers = range (1,11)
-    return render_template("body.html", object = numbers)
+    names =["tom", "osvaldo", "guile", "tommy"]
+    return render_template("body.html", object = names)
 
 
 # run this app in debug mode on your local.
 if __name__== "__main__":
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
-
+    app.run(debug=True, host='0.0.0.0', port=80)
