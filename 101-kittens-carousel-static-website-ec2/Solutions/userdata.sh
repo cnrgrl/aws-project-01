edb73b72d306c5d091315f047e457927c474f1f4
caner <66187631+cnrgrl@users.noreply.github.com>
2024-06-02 13:46:54 +0200

    improve

--- 101-kittens-carousel-static-website-ec2/Solutions/userdata.sh
+++ 101-kittens-carousel-static-website-ec2/Solutions/userdata.sh
@@ -1,12 +1,20 @@
-#! /bin/bash
-dnf update -y
-dnf install httpd -y
-FOLDER="https://raw.githubusercontent.com/awsdevopsteam/101-cfn-static-website-ec2/master/static-web"
-cd /var/www/html
-wget ${FOLDER}/index.html
-wget ${FOLDER}/cat0.jpg
-wget ${FOLDER}/cat1.jpg
-wget ${FOLDER}/cat2.jpg
-wget ${FOLDER}/cat3.png
-systemctl start httpd
-systemctl enable httpd
+#!/bin/bash
+
+# Update and install httpd
+sudo dnf update -y && sudo dnf install httpd -y
+
+# Define the folder and change directory
+folder="https://raw.githubusercontent.com/awsdevopsteam/101-cfn-static-web
+   -ec2/master/static-web"
+cd /var/www/html || exit
+
+# Download the files using wget
+sudo wget ${folder}/index.html
+sudo wget ${folder}/cat0.jpg
+sudo wget ${folder}/cat1.jpg
+sudo wget ${folder}/cat2.jpg
+sudo wget ${folder}/cat3.png
+
+# Start and enable httpd service
+sudo systemctl start httpd
+sudo systemctl enable httpd
