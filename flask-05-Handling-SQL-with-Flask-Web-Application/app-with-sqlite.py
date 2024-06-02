8070cf9d238662534d598d53a794e3db73cf9c8e
caner <66187631+cnrgrl@users.noreply.github.com>
2024-06-02 13:47:03 +0200

    improve

--- flask-05-Handling-SQL-with-Flask-Web-Application/app-with-sqlite.py
+++ flask-05-Handling-SQL-with-Flask-Web-Application/app-with-sqlite.py
@@ -1,88 +1,64 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./email.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-with app.app_context():
-    drop_table = text('DROP TABLE IF EXISTS users;')
-    users_table = text(""" 
-    CREATE TABLE users(
-    username VARCHAR NOT NULL PRIMARY KEY,
-    email VARCHAR);
-    """)
-    data = text("""
-    INSERT INTO users
-    VALUES
-        ("caner", "caner@amazon.com"),
-        ("hasan", "hasan@google.com"),
-        ("ibrahim", "ibrahim@bmw.com"),
-        ("ramazan", "ramazan@mercedes.com"),
-	    ("aynur", "aynur@porche.com"),
-        ("fatih", "fatih@huwaei.com");
-        """)
-    db.session.execute(drop_table)
-    db.session.execute(users_table)
-    db.session.execute(data)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    users = [
+        User(username='caner', email='caner@amazon.com'),
+        User(username='hasan', email='hasan@google.com'),
+        User(username='ibrahim', email='ibrahim@bmw.com'),
+        User(username='ramazan', email='ramazan@mercedes.com'),
+        User(username='aynur', email='aynur@porche.com'),
+        User(username='fatih', email='fatih@huwaei.com'),
+    ]
+    db.session.bulk_save_objects(users)
     db.session.commit()
 
-def find_emails(keyword):
-    with app.app_context():
-        query = text(f"""
-        SELECT * FROM users WHERE username like '%{keyword}%';
-        """)
-        result = db.session.execute(query)
-        user_emails = [(row[0], row[1]) for row in result]
-        if not any(user_emails):
-            user_emails = [("Not Found", "Not Found")]
-        return user_emails
-
-def insert_email(name,email):
-    with app.app_context():
-        query = text(f"""
-        SELECT * FROM users WHERE username like '{name}'
-        """)
-        result = db.session.execute(query)
-        response = ''
-        if len(name) == 0 or len(email) == 0:
-            response = 'Username or email can not be empty!!'
-        elif not any(result):
-            insert = text(f"""
-            INSERT INTO users
-            VALUES ('{name}', '{email}');
-            """)
-            result = db.session.execute(insert)
-            db.session.commit()
-            response = text(f"User {name} and {email} have been added successfully")
-        else:
-            response = text(f"User {name} already exist")
-        return response
 @app.route('/', methods=['GET', 'POST'])
 def emails():
-    with app.app_context():
-        if request.method == 'POST':
-            user_app_name = request.form['user_keyword']
-            user_emails = find_emails(user_app_name)
-            return render_template('emails.html', name_emails=user_emails, keyword=user_app_name,   show_result=True)
-        else:
-            return render_template('emails.html', show_result=False)
+    if request.method == 'POST':
+        user_app_name = request.form['user_keyword']
+        user_emails = User.query.filter(User.username.ilike(f'%{user_app_name}%')).all()
+        if not user_emails:
+            flash('No results found.')
+        return render_template('emails.html', name_emails=user_emails, keyword=user_app_name, show_result=True)
+    else:
+        return render_template('emails.html', show_result=False)
+
 @app.route('/add', methods=['GET', 'POST'])
 def add_email():
-    with app.app_context():
-        if request.method == 'POST':
-            user_app_name = request.form['username']
-            user_app_email = request.form['useremail']
-            result_app = insert_email(user_app_name, user_app_email)
-            return render_template('add-email.html', result_html=result_app, show_result=True)
+    if request.method == 'POST':
+        user_app_name = request.form['username']
+        user_app_email = request.form['useremail']
+        if not user_app_name or not user_app_email:
+            flash('Username or email can not be empty!!')
         else:
-            return render_template('add-email.html', show_result=False)
+            user = User.query.filter_by(username=user_app_name).first()
+            if user:
+                flash(f'User {user_app_name} already exist')
+            else:
+                user = User(username=user_app_name, email=user_app_email)
+                db.session.add(user)
+                db.session.commit()
+                flash(f"User {user_app_name} and {user_app_email} have been added successfully")
+        return render_template('add-email.html', show_result=True)
+    else:
+        return render_template('add-email.html', show_result=False)
 
-
-# - Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__=='__main__':
     app.run(debug=True)
-    #app.run(host='0.0.0.0', port=80)
-
-# https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/
